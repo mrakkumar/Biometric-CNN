524a70d2e9cc45791fdeaa0df5d6fcbb8b41259f
nnarenraju <nnarenraju@gmail.com>
2020-08-22 19:26:03 +0530

    Added a basic read and cleanup routine for model_parser .txt input

--- source/model_parser.py
+++ source/model_parser.py
@@ -24,3 +24,56 @@ Documentation: NULL
 
 """
 
+import re
+import json
+
+class ModelParser():
+
+    def __init__(self, modelconfig):
+        # Model object as returned after parsing
+        self.model = None
+        # Config .txt file for model
+        self.config = modelconfig
+        # Path to model save location
+        # Model not saved by default
+        self.modelpath = None
+        # Status of model (compiled/uncompiled)
+        self.compiled = False
+    
+    def read_input(self):
+        # Read input file and strip newline characters
+        with open(self.config, 'r') as file:
+            data = file.read()
+            # Clean up the model file
+            # Remove comments and carriage returns
+            re.sub('^#.*?\n', '', data)
+            data.replace('\n', '')
+                
+        # Loading the above data into a dict obj
+        layers = json.loads(data)
+        
+        # Stroring the above dict as variables
+        # Use this only if exec will not cause problems
+        """
+        for (n, v) in layers.items():
+            exec('%s=%s' % (n, repr(v)))
+        """
+        
+        # Returns all layers as dict object
+        return layers
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
