4c650c859760995dc6d27ae28631b729a7d5b8c9
nnarenraju <nnarenraju@gmail.com>
2020-08-22 18:03:39 +0530

    Started the model_parser module

---
+++ source/model_parser.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+foo.py
+    Short abstract of foo.py
+    Additional description
+
+Created on Sat Aug 21 17:12:22 2020
+
+__author__      = nnarenraju
+__copyright__   = Copyright 2020, Project Name
+__credits__     = nnarenraju
+__license__     = Apache License 2.0
+__version__     = 1.0.0
+__maintainer__  = nnarenraju
+__email__       = nnarenraju@gmail.com
+__status__      = ['inProgress', 'inUsage', 'Archived', 'Debugging']
+
+
+Github Repository: NULL
+
+Documentation: NULL
+
+"""
+
