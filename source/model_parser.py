6a03be2fc59fe7bc9a14c7d205bead498bb3b8c0
nnarenraju <nnarenraju@gmail.com>
2020-08-22 20:41:49 +0530

    Added a parser to the model_parser module

--- source/model_parser.py
+++ source/model_parser.py
@@ -26,12 +26,19 @@ Documentation: NULL
 
 import re
 import json
+import inspect
+from tensorflow.keras import layers, models, optimizers, initializers
 
 class ModelParser():
 
-    def __init__(self, modelconfig):
+    def __init__(self, modelconfig, nclasses, imgshape, seed=None):
         # Model object as returned after parsing
         self.model = None
+        self.layers = None
+        self.seed = seed
+        # Dataset params
+        self.nclasses = nclasses
+        self.imgshape = imgshape
         # Config .txt file for model
         self.config = modelconfig
         # Path to model save location
@@ -50,7 +57,7 @@ class ModelParser():
             data.replace('\n', '')
                 
         # Loading the above data into a dict obj
-        layers = json.loads(data)
+        self.layers = json.loads(data)
         
         # Stroring the above dict as variables
         # Use this only if exec will not cause problems
@@ -58,14 +65,51 @@ class ModelParser():
         for (n, v) in layers.items():
             exec('%s=%s' % (n, repr(v)))
         """
+    
+    def make_model(self):
+        """
+        Make a tf model using modelconfig
+        """
         
-        # Returns all layers as dict object
-        return layers
+        # Initializer and first_layer params
+        # Initializer is not inspected as seed is always provided/no other params possible
+        initializer = getattr(initializers, self.layers[0]['kernel_initializer'])(seed=self.seed)
+        # Start model scope
+        model = models.Sequential()
+        
+        # pop the optimizer if present
+        optimizer = None
+        for n, layer in enumerate(self.layers):
+            if layer['layername'] in dir(optimizers):
+                optimizer = getattr(optimizers, layer['layername'])
+                opt_params = self.layers.pop(n)
+                del opt_params['layername']
+                break
+        else:
+            optimizer = None
+        
+        # Iterating through all layers
+        for layer in self.layers:
+            tf_layer = getattr(layers, layer['layername'])
+            del layer['layername']
+            
+            # Sanity check
+            allparams = inspect.getfullargspec(tf_layer).args
+            if not set(layer.keys()) <= set(allparams):
+                raise NameError("One or more params passed to layer does not exist.")
+            # First layer exception
+            if 'kernel_initializer' in layer.keys():
+                layer['kernel_initializer'] = initializer
+            
+            # Pass all params to the tf layer function
+            # as layer is a dict, params are automatically mapped by **kwargs
+            model.add(tf_layer(layer))
+        
+        # Run the optimizer if requested
+        if optimizer:
+            optimizer(opt_params)
     
-    
-    
-    
-    
+        return model
     
     
     
