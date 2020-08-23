29f10cb2e41475cd28ad2de933f3ddbc54211796
nnarenraju <nnarenraju@gmail.com>
2020-08-24 02:17:57 +0530

    Major bug fixes, updated __doc__, super_model support

--- source/run.py
+++ source/run.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 
 """
-main.py
-    End-user interface for the biometric-CNN project 
+run.py
+    central control to different modules of the biometric-CNN project
 
 Created on Wed Aug 19 00:48:03 2020
 
-__author__      = 
+__author__      = nnarenraju
 __copyright__   = Copyright 2020, biometric-CNN
 __credits__     = 
 __license__     = 
 __version__     = 1.0.0
-__maintainer__  = nnarenraju
+__maintainer__  = 
 __email__       = 
-__status__      = inProgress
+__status__      = inProgress, inUsage
 
 
 Github Repository: https://github.com/mrakkumar/Biometric-CNN
@@ -38,36 +38,61 @@ from model_parser import ModelParser
 import tensorflow as tf
 from keras.models import load_model
 
+
 class Run():
     
     """
     params:
-
-        modelfile - string    - Points to model .txt file
-        augment   - boolean   - Do or ignore augment method
-        valsize   - float     - Proportion of validation set (min:0.0, max:0.99)
-        datadir   - string    - PATH to dataset folder (w class-subdir)
-        imgcount  - integer   - Count of total number of images in datadir
-        imgfile   - string    - PATH to single image file
-        filetype  - string    - File extension of img files (def: jpg)
-        train     - objects   - Image objects (as defined by TF) of training set
-        vals      - objects   - Image objects (as defined by TF) of validation set
-        savefeat  - Boolean   - Save CNN features
-        imread    - string    - Image read method (refer imgread.py)
-        seed      - int       - Seed value for operation
-        savemod   - Boolean   - Save the (un)compiled model in HDF5 format
         
-    methods:
+        # MODEL PARAMS
+        special     - boolean   - enables/disables super_model usage
+        Model       - object    - holds a model_parser object
+        modelfile   - string    - points to model object .pkl file
+        modelconfig - string    - points to modelconfig .txt file
+        seed        - int       - Seed value for operation
+        savemod     - Boolean   - Save the (un)compiled model in HDF5 format
+        
+        # DATA PARAMS
+        datadir     - string    - PATH to dataset folder (w class-subdir)
+        dataset     - object    - tf.data.Dataset object
+        imgcount    - int       - number of images present in dataset
+        batchsize   - int       - size of batch for performace method
+        filetype    - string    - File extension of img files (def: jpg)
+        imgshape    - tuple     - (height, width, 1) from input image
+        nclasses    - int       - number of classes present in dataset
+        height      - int       - height in pixels of input image
+        width       - int       - width in pixels of input image
+        
+        # PRE-PROCESSING (maintains folder hierarchy)
+        prepdir     - string    - path to pre-processed dataset
+        augment     - boolean   - augments the pre-processed dataset
+        
+        # SPLIT PARAMS
+        train       - objects   - Image objects (as defined by TF) of training set
+        vals        - objects   - Image objects (as defined by TF) of validation set
+        valsize     - float     - Proportion of validation set (min:0.0, max:0.99)
+        
+    public methods:
+        
+        make_dataset  - creates and shuffles the dataset as tf.data.Dataset object
+        split         - splits dataset into train and vals based on valsize
+        make_pair     - make (img, label) pairs using dataset
+        performance   - configure performace (tf methods)
+        get_model     - make and get an uncompiled model
 
-        read      -        - 
-
-    Creates an object that can be input as a Structure to a c-types 
-    function written to call a c-function.
+    Central control to different modules of the biometric-CNN project.
+    Methods which are on beta have not been included in this __doc__
 
     """
 
     def __init__(self, datadir, modelfile=None, modelconfig=None):
-
+        
+        ### Logging params ###
+        self.log = logging.getLogger(__name__)        
+        
+        # special (enables super_model)
+        self.special = False
+        
         # Model params
         self.Model = None
         self.modelfile = modelfile
@@ -83,27 +108,31 @@ class Run():
         self.filetype = "jpg"
         self.imgshape = None
         self.nclasses = None
+        # get dim automatically
+        # currently manual
+        self.height = None
+        self.width = None
 
         # Pre-processing params
         self.prepdir = None
         self.augment = False
         
-        # Fitting params
+        # Split params
         self.train = None
         self.vals = None
         self.valsize = 0.2
         self.savefeat = False
         
-        # Special
+        # experimental
         self.imread = None
         self.imgfile = None
         
         ### TF Params ###
         self.AUTOTUNE = tf.data.experimental.AUTOTUNE
-        
 
     def read(self):
-        """ special function - under experimentation """
+        """ under experimentation """
+        self.log.debug('method "read" currently under debugging status has been accessed.')
         return getattr(im, self.imread)(self.imgfile)
 
     def img_count(self):
@@ -113,6 +142,7 @@ class Run():
         self.img_count()
         list_ds = tf.data.Dataset.list_files(str(self.datadir/'*/*'), shuffle=False)
         self.dataset = list_ds.shuffle(self.imgcount, reshuffle_each_iteration=False)
+        self.log.info("Dataset has been created and shuffled")
         
     def get_classes(self):
         return numpy.array(sorted([item.name for item in self.datadir.glob('*') if item.name != "LICENSE.txt"]))
@@ -120,7 +150,8 @@ class Run():
     def split(self):
         val_size = int(self.imgcount * self.valsize)
         self.train = self.dataset.skip(val_size)
-        self.vals = self.dataset.take(val_size)  
+        self.vals = self.dataset.take(val_size)
+        self.log.info("Dataset split into training and validation sets")
 
     ### converts a file path to an (img, label) pair ###
     
@@ -136,9 +167,7 @@ class Run():
         # convert the compressed string to a 3D uint8 tensor
         img = tf.image.decode_jpeg(img, channels=3)
         # resize the image to the desired size
-        image, label = self.train.take(1)
-        img_height, img_width, _ = image.numpy().shape
-        return tf.image.resize(img, [img_height, img_width])
+        return tf.image.resize(img, [self.height, self.width])
 
     def process_path(self, file_path):
         label = self.get_label(file_path)
@@ -152,6 +181,7 @@ class Run():
         # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
         self.train = self.train.map(self.process_path, num_parallel_calls=self.AUTOTUNE)
         self.vals = self.vals.map(self.process_path, num_parallel_calls=self.AUTOTUNE)
+        self.log.info("Training and validation data converted to (img, label) pairs")
 
     def get_pairs(self, dataset):
         # Use if fitting with (data, label) input
@@ -163,22 +193,24 @@ class Run():
     def configure_for_performance(self, ds):
         ds = ds.cache()
         ds = ds.shuffle(buffer_size=1000)
-        ds = ds.batch(self.batch_size)
+        ds = ds.batch(self.batchsize)
         ds = ds.prefetch(buffer_size=self.AUTOTUNE)
         return ds
 
     def performance(self):
         self.train = self.configure_for_performance(self.train)
         self.vals = self.configure_for_performance(self.vals)
+        self.log.info("Configure for performance tf method have been called")
 
     ### Pre-processing the dataset ###
 
     def preprocess(self):
-        self.prepdir = prep.preprocess_images(self.datadir)
+        prepdir = prep.preprocess_images(self.datadir)
         # Sanity check
         if os.path.exists(self.prepdir):
             raise ValueError("Prep folder not created.")
-
+        self.prepdir = pathlib.Path(prepdir)
+        self.log.info("Pre-processing completed and path received")
     
     ########################### MODEL MANIPULATION ################################
     # This section contains function definitions pertaining to model reading from 
@@ -190,18 +222,20 @@ class Run():
 
     def get_model(self):
         # Get uncompiled model object from ModelParser
-        self.nclasses = len(self.get_classes)
-        self.imgshape = self.train.take(1)[0].shape
+        self.nclasses = len(self.get_classes())
+        self.imgshape = (self.height, self.width, 1)
         
         # Sanity check
         self.is_exists(self.modelconfig)
         # Connect to model_parser
-        mp = ModelParser(self.modelconfig, self.nclasses, self.imgshape, self.seed)
+        mp = ModelParser(self.modelconfig, self.nclasses, self.imgshape, self.seed, \
+                         special=self.special)
         # Reads, cleans and loads the model config file
         mp.read_input()
         # Make an uncompiled model
         mp.make_model()
         self.Model = mp
+        self.log.info("Model object has been successfully created")
 
     def is_exists(self, filename):
         if not os.path.exists(filename):
@@ -226,16 +260,21 @@ class Run():
         self.Model.modelpath = 'saved_models/models/model_{}.h5'.format(str(dt.datetime.now().time()))
         self.Model.model.save(self.Model.modelpath)
         del self.Model.model #remove this if needed
+        self.log.warning("Model has been deleted from object after saving. Load for reuse.")
         # Save ModelClass object
         self.write_pkl(self.Model, filename='saved_models/model_{}.pkl'.format(str(dt.datetime.now().time())))
+        self.log.info("Model has been saved under saved_models/")
 
     def load(self):
-        # Sanity check
+        # Loading a compiled/raw model from HDF5
+        # modelfile is a .pkl file with info about model stored
         self.is_exists(self.modelfile)
-        mc = self.read_pkl(self.modelfile)
-        self.is_exists(mc.modelpath)
-        mc.model = load_model(mc.modelpath)
-        self.Model = mc
+        mod = self.read_pkl(self.modelfile)
+        # modelpath points to the unique HDF5 file
+        self.is_exists(mod.modelpath)
+        mod.model = load_model(mod.modelpath)
+        self.Model = mod
+        self.log.info("Model successfully loaded from modelfile")
 
 
 
